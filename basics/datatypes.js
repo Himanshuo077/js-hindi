621548033ca9298552d64982bbdd18429873dbcf
Himanshuo077 <152697632+Himanshuo077@users.noreply.github.com>
2025-02-28 19:01:30 +0000

    config done

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+let name = "Sumit"
+let age = 24
+let isLoggin = true
+let state;
+
+// number => 2 to power of 53
+// bigint
+// String =>""
+// boolean => true/false
+// null => standalone value
+// undesfined
+// symbol =>unique
+
+console.table([name,age,isLoggin,state])
+console.log(typeof name);
+console.log(typeof null);
+

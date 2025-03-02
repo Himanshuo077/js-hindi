1957222d4b12d7764daf6b0e54af06d42c696f49
Himanshuo077 <152697632+Himanshuo077@users.noreply.github.com>
2025-03-02 18:10:50 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId= 14423
+let accountEmail = "himanshusingh7294@gmail.com"
+var accountPassword= "12345"
+accountCity= "Muzaffarpur"
+
+//accountId = 2
+accountEmail = "hk@gmail.com"
+accountPassword ="8765"
+accountCity = "sitamadhi"
+
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);

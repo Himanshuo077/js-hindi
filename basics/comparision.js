1b3062c2cdcc2a4e9d5670726bcbf763d54ed63a
Himanshuo077 <152697632+Himanshuo077@users.noreply.github.com>
2025-05-14 01:06:33 +0530

    container config done

--- basics/comparision.js
+++ basics/comparision.js
@@ -1,2 +1,22 @@
 let Newname='33abc';
 console.log(typeof(Newname))
+console.log(Newname);
+
+// console.log("2">1);
+// console.log("2"==1);
+// console.log("2">=1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log("2" === 2);
+
+
+
+
+
+
---
+++ basics/datatype_summary.js
@@ -0,0 +1,26 @@
+
+//Primitve
+// string, number, null, Boolean, undefined, BigInt, Symbol 
+
+let fullname = 'sonu';
+console.log(fullname);
+
+let Serial_number = 1;
+console.log(Serial_number);
+
+console.log(null);
+console.log(undefined);
+
+let isLoggin = true;
+console.log(isLoggin);
+
+let Bignumber = 6736135178135;
+console.log(Bignumber);
+
+
+
+
+//Non-primitive
+//Reference
+
+

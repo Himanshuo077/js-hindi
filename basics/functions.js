50501872e3e59dcecab2b70cb5e0ec4804748b5d
Himanshuo077 <152697632+Himanshuo077@users.noreply.github.com>
2025-10-13 23:16:00 +0530

    container config done

--- basics/functions.js
+++ basics/functions.js
@@ -12,13 +12,36 @@
 // console.log(result);
 
 
-function loginuser(username = "Himanshu") {
-    if (!username) {
-        console.log("Please enter aa username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginuser(username = "Himanshu") {
+//     if (!username) {
+//         console.log("Please enter aa username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
 // console.log(loginuser("Sujit"));
-console.log(loginuser("Mayank"));
+// console.log(loginuser("Mayank"));
+
+
+// const user = {
+//     name : "Sonu",
+//     price : 200
+// }
+
+// function handleObject(anyone) {
+//     console.log(`username is: ${anyone.name}, price is: ${anyone.price}`);
+    
+// }
+
+// handleObject(user)
+
+const array = [10, 20, 30, 40]
+
+function returnarray(getarray) {
+    return getarray[2]
+}
+
+// console.log(returnarray(array));
+console.log(returnarray([30,40,50,60]));
 
